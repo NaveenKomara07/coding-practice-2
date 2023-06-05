930133023895ffec21d4f4aa1b50284b4fe868d2
NaveenKomara07 <naveenkomara0525@gmail.com>
2023-06-05 13:14:12 +0000

    solution

---
+++ index.js
@@ -0,0 +1,8 @@
+let addDays = require("date-fns/addDays");
+
+function days(d) {
+  let date = addDays(new Date(2020, 7, 22), d);
+  console.log(`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`);
+}
+
+days(2);
